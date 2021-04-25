c4b83f07066b3b7d93ebce00bbe45fae0913701d
ka1ex <kalexadamson@hotmail.com>
2021-04-25 14:13:59 -0500

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+    return cats
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+function appendCat(name){
+    let BroomBroom = [...cats, name]
+    return BroomBroom
+}
+function prependCat(name){
+    let cats3 = [name, ...cats]
+    return cats3
+} 
+function removeLastCat(){
+ let cats4 = cats.slice(0, -1)
+return cats4
+}
+function removeFirstCat(){
+ let cats5 = cats.slice(1)
+ return cats5
+}
